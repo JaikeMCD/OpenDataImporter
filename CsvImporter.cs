f8256e701e63576d005ac3567a1793aa5e6e8e1a
Simon Lang <simon@tinybra.in>
2018-04-26 15:25:09 +1000

    Added scratch CsvImporter and ImportScript.

---
+++ CsvImporter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using CsvHelper;
+
+namespace Mcd.OpenData
+{
+    public class CsvImporter
+    {
+        public List<dynamic> Records { get; set; }
+
+        public static CsvImporter OpenCsv(string path)
+        {
+            CsvImporter importer = new CsvImporter();
+            importer.Open(path);
+            return importer;
+        }
+
+        public void Open(string path)
+        {
+            using (var reader = File.OpenText(path))
+            using (var csv = new CsvReader(reader))
+            {
+                csv.Configuration.PrepareHeaderForMatch = header => header.Replace(" ", "_");
+                Records = csv.GetRecords<dynamic>().ToList();
+            }
+        }
+    }
+}
---
+++ ImportScript.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+namespace Mcd.OpenData
+{
+    public class OpenDataRecord
+    {
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public string Field1 { get; set; }
+        public string Field2 { get; set; }
+        public string Field3 { get; set; }
+        public string Field4 { get; set; }
+        public string Field5 { get; set; }
+        public string Field6 { get; set; }
+        public string Field7 { get; set; }
+        public string Field8 { get; set; }
+        public string Field9 { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitutde { get; set; }
+    }
+
+    public class ImportScriptArgs
+    {
+        public dynamic src;
+        public OpenDataRecord dst;
+    }
+
+    public static class ImportScript
+    {
+        public static async Task Run()
+        {
+            try
+            {
+                var scriptContent = "dst.Title = src.Title";
+
+                dynamic expando = new ExpandoObject();
+                expando.Title = "Wangaratta";
+
+                var refs = new List<MetadataReference>
+                {
+                    MetadataReference.CreateFromFile(typeof(Microsoft.CSharp.RuntimeBinder.RuntimeBinderException).GetTypeInfo().Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(System.Runtime.CompilerServices.DynamicAttribute).GetTypeInfo().Assembly.Location)
+                };
+
+                ScriptOptions options = ScriptOptions.Default
+                                                     .AddReferences(refs);
+
+                var script = CSharpScript.Create(
+                    scriptContent,
+                    options: options,
+                    globalsType: typeof(ImportScriptArgs)
+                );
+
+                script.Compile();
+
+                var g = new ImportScriptArgs { src = expando, dst = new OpenDataRecord() };
+
+                var r = await script.RunAsync(g);
+
+                Console.WriteLine(r.ReturnValue);
+
+
+                /*
+                ScriptRunner<int> runner = script.CreateDelegate();
+
+                for (int i = 0; i < 10; ++i)
+                {
+                    int r = await runner(new ImportScriptArgs { x = i, y = i });
+
+                    Console.WriteLine("{0} -> {1}", i, r);
+                }
+                */
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Import Script: {0}", e);
+            }
+        }
+    }
+}
