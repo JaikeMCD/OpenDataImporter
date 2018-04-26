d35d2a0000f5168804fb3d652aa972185c332a45
Simon Lang <simon@tinybra.in>
2018-04-27 08:40:37 +1000

    Merged CKAN stuff to one file.

--- CKAN/Client.cs
+++ CKAN/Client.cs
@@ -5,6 +5,23 @@ using System.Threading.Tasks;
 
 namespace Mcd.OpenData.CKAN
 {
+    public class Resource
+    {
+        public class Result
+        {
+            public Guid package_id { get; set; }
+            public Guid id { get; set; }
+            public Guid revision_id { get; set; }
+            public Uri url { get; set; }
+            public string format { get; set; }
+            public DateTime last_modified { get; set; }
+            public bool datastore_active { get; set; }
+        }
+
+        public bool success { get; set; }
+        public Result result { get; set; }
+    }
+
     public class Client
     {
         private HttpClient client = new HttpClient();
